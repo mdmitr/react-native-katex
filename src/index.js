59a9ff03eb5ee1732b3bdd887c7a62af031e09f0
Mikhail Dmitrievskiy <mdmitr@gmail.com>
2019-05-11 21:14:41 +0500

    switching WebView from react-native to react-native-webview

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { WebView, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 import katexStyle from './katex-style';
 import katexScript from './katex-script';
